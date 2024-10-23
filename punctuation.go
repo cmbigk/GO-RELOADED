287e3496d998f77afbdb1ae8afdc9fbe2deae58d
cmbigk <bigk89865@gmail.com>
2024-10-23 13:55:18 +0300

    okk! I still need to do Punctuation.

---
+++ punctuation.go
@@ -0,0 +1,15 @@
+package main
+
+func PunPun(s string) string {
+	Punlay := ".,!?;:"
+	result := ""
+
+	for _, char := range Punlay {
+		isPunlay := false
+		if char == s {
+			isPunlay = true
+			break
+		}
+	}
+
+}
