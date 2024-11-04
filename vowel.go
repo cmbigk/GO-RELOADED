5b1d7544f1b1d8f09532d9710abdf499a3d888ac
cmbigk <bigk89865@gmail.com>
2024-11-04 20:44:19 +0200

    just editing!

--- vowel.go
+++ vowel.go
@@ -1,5 +1,6 @@
 package main
 
+// IsVowelOrH checks if the provided character is a vowel (a, e, i, o, u) or the letter 'h' (both uppercase and lowercase).
 func IsVowelOrH(char rune) bool {
 	vowels := "aeiouAEIOUhH"
 	for _, each := range vowels {
@@ -11,11 +12,15 @@ func IsVowelOrH(char rune) bool {
 }
 
 func ReplaceAWithAn(words []string) []string {
-
 	for i := 0; i < len(words)-1; i++ {
-		if words[i] == "a" && len(words[i+1]) > 0 && IsVowelOrH(rune(words[i+1][0])) {
-
-			words[i] = "an"
+		// Check if the current word is "a" or "A" and the next word starts with a vowel or 'h'
+		if (words[i] == "a" || words[i] == "A") && len(words[i+1]) > 0 && IsVowelOrH(rune(words[i+1][0])) {
+			// Replace "a" with "an" or "A" with "An" based on the case
+			if words[i] == "a" {
+				words[i] = "an"
+			} else if words[i] == "A" {
+				words[i] = "An"
+			}
 		}
 	}
 	return words
